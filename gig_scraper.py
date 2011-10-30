24d97c2deff176203aee12ffd7adc92f39599b55
dm03514 <dm03514@dm03514-Inspiron-1545.(none)>
2011-10-30 11:34:33 -0400

    Addede git ignore and small changes on scraper

--- gig_scraper.py
+++ gig_scraper.py
@@ -1,19 +1,21 @@
 from BeautifulSoup import BeautifulSoup
-from cookielib import CookieJar
+import config
 import re
+import smtplib
 from urllib import urlopen
-import urllib2
 
 def main():
   gig_areas_list = [
     'baltimore',
     'washingtondc',
   ]
-  get_posting_urls(gig_areas_list)
+  gig_urls_list = get_posting_urls(gig_areas_list)
+  get_relevent_gigs(gig_urls_list)
 
 def get_posting_urls(gig_areas_list):
   """
   Scrape a list of gig urls for relevent freelance gigs.
+  @return list gig urls
   """
   for gig_area in gig_areas_list:
     url = 'http://%s.craigslist.org/cpg/' % (gig_area)
@@ -21,25 +23,27 @@ def get_posting_urls(gig_areas_list):
     soup = BeautifulSoup(f.read())
     # Get just the postings
     posting_urls_result_set = soup.findAll('a', href=re.compile(url+'\d+.html'))
-    gig_urls_list = [tag.attrs[0][1] for tag in posting_urls_result_set]
-    get_relevent_gigs(gig_urls_list)
-
+    # combine all areas instead of returning right here
+    return [tag.attrs[0][1] for tag in posting_urls_result_set]
+    
 def get_relevent_gigs(gig_urls_list):
   """
   Get postings that contain any of my skills.  Requires a list of posting urls to try.
+  @return 
   """
   import ipdb; ipdb.set_trace()
-  my_skills_list = ['php', 'python', 'django', 'css', 'html', 'javascript']
+  my_skills_list = ['php', 'python', 'django', 'css', 'html', 'javascript',]
   matches_list = []
   for gig_url in gig_urls_list:
     f = urlopen(gig_url)
-    posting_str = f.read().lower()
+    soup = BeautifulSoup(f.read())
+    body_str = soup.find('div', id='userbody').text.lower()
     gig_dict = {'url': gig_url, 'skills': []}
     for skill in my_skills_list:
-      if skill in posting_str:
+      if skill in body_str:
         gig_dict['skills'].append(skill)
     if gig_dict['skills']:
-      matches.append(gig_dict) 
+      matches_list.append(gig_dict) 
   if matches_list:
     send_email(matches_list)
 
@@ -47,7 +51,20 @@ def send_email(matches_list):
   """
   Send an email with any matching gigs.
   """
-  pass
+  to = 'dm03514@gmail.com'
+  gmail_user = ''
+  gmail_pwd = ''
+  smtpserver = smtplib.SMTP("smtp.gmail.com",587)
+  smtpserver.ehlo()
+  smtpserver.starttls()
+  #smtpserver.ehlo
+  smtpserver.login(gmail_user, gmail_pwd)
+  header = 'To:' + to + '\n' + 'From: ' + gmail_user + '\n' + 'Subject:testing \n'
+  print header
+  msg = header + '\n this is test msg from mkyong.com \n\n'
+  smtpserver.sendmail(gmail_user, to, msg)
+  print 'done!'
+  smtpserver.close()
 
 if __name__ == '__main__':
   main() 
